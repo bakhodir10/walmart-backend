5652d7223fcf85c668d420d0ac4880add322dc03
Abiel Brhane <abielberaki1@gmail.com>
2018-07-15 00:54:36 -0600

    fix user auth

--- routes/user-auth-route.js
+++ routes/user-auth-route.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var userAuthService = require('../app/user/user.Auth.service');
 
-
 /* GET users listing. */
 router.post('/login', userAuthService.login);
 
