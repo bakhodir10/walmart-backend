922de63b632ecf0ee4d94eec6d215651cdf31b90
Samicoen <skflay@mum.edu>
2018-07-16 20:31:02 -0500

    product verification

--- routes/product-route.js
+++ routes/product-route.js
@@ -4,18 +4,21 @@ var productService = require('../app/product/product.service');
 var userAuthService = require('../app/user/user.Auth.service');
 
 /* GET users listing. */
-router.get('/', productService.list);
+router.get('/', userAuthService.checkForToken, userAuthService.verifyToken,productService.list);
 
 /* POST a user. */
-router.post('/', productService.save);
+router.post('/', userAuthService.checkForToken, userAuthService.verifyToken,(req,res,next)=>{req.x=['manager','employee'];next();},
+userAuthService.hasRole,productService.save);
 
 /* GET a user. */
-router.get('/:id', productService.findOne);
+router.get('/:id', userAuthService.checkForToken, userAuthService.verifyToken,productService.findOne);
 
 /* DELETE a user. */
-router.delete('/:id', productService.delete);
+router.delete('/:id', userAuthService.checkForToken, userAuthService.verifyToken,(req,res,next)=>{req.x=['manager','employee'];next();},
+userAuthService.hasRole, productService.delete);
 
 /* UPDATE a user. */
-router.put('/:id', productService.update);
+router.put('/:id', userAuthService.checkForToken, userAuthService.verifyToken,(req,res,next)=>{req.x=['manager','employee'];next();},
+userAuthService.hasRole, productService.update);
 
 module.exports = router;
